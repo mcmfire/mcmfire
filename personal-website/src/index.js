ec9906ceaf48368c9dcfe5a5e1efd10638f1a9ab
Marc Chester Masakayan <141996880+mcmfire@users.noreply.github.com>
2023-10-21 17:10:02 +0800

    Add home page
    Created a Home component and added it to the routes.

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <div className="app">
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<></>}/>
+                <Route exact path="/" element={<Home/>}/>
             </Routes>
         </BrowserRouter>
     </div>
