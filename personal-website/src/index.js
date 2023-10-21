b82d782fdaaffb820d863f7698af8f0f1d0d9d11
Marc Chester Masakayan <141996880+mcmfire@users.noreply.github.com>
2023-10-21 17:21:34 +0800

    Add .scss files for styling
    Configured webpack and package.json to setup and support SASS.
    
    Expanded the root container and homepage container to fill the whole document.

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./index.scss";
 
 const Home = lazy(() => import("./pages/home/home"));
 
