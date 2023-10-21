4bc10ab2d09938f5c89669730b0e8246fa92d4fb
Marc Chester Masakayan <141996880+mcmfire@users.noreply.github.com>
2023-10-21 17:13:09 +0800

    Implement lazy importing
    Performed code-splitting by lazy importing components and added a Suspense fallback to display React fragment while the component was not yet loaded.

--- personal-website/src/index.js
+++ personal-website/src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home/home"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <div className="app">
         <BrowserRouter>
+        <Suspense fallback={<></>}>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
+                <Route path="*" element={<Navigate to="/"/>}/>
             </Routes>
+        </Suspense>
         </BrowserRouter>
-    </div>
+    </div> 
 );
